1197d1b1a0ba2d7594bf19d429f5451da6b9e5f2
RabieLakhdar <mac@rafa.local>
2020-02-02 14:34:57 +0100

    export static data

--- api/controller/data.js
+++ api/controller/data.js
@@ -1,21 +1,8 @@
 const util = require("../util/globalFunc");
+const {
+   genderList
+  } = require("../util/data");
 
-const genderList = [
-    {value: 'Girl'},
-    {value: 'GirlFriend'},
-    {value: 'Boy'},
-    {value: 'BoyFriend'},
-    {value: 'SexFriend'},
-    {value: 'Family " Mother, Father"'},
-    {value: 'Family " Mother, Father & Brother "'},
-    {value: 'Family " Mother, Father & Sister "'},
-    {value: 'Family " Mother, Father & Sister "'},
-    {value: 'Family " Mother, Father, Brother & Sister "'},
-    {value: 'Grand Father'},
-    {value: 'Grand Mather'},
-    {value: 'Cousin'},
-    {value: 'Neighboor'},
-  ];
 
 exports.getAllgender = (req, res, next) => {
     res
---
+++ api/util/data.js
@@ -0,0 +1,20 @@
+const genderList = [
+    {value: 'Girl'},
+    {value: 'GirlFriend'},
+    {value: 'Boy'},
+    {value: 'BoyFriend'},
+    {value: 'SexFriend'},
+    {value: 'Family " Mother, Father"'},
+    {value: 'Family " Mother, Father & Brother "'},
+    {value: 'Family " Mother, Father & Sister "'},
+    {value: 'Family " Mother, Father & Sister "'},
+    {value: 'Family " Mother, Father, Brother & Sister "'},
+    {value: 'Grand Father'},
+    {value: 'Grand Mather'},
+    {value: 'Cousin'},
+    {value: 'Neighboor'},
+  ];
+
+  module.exports = {
+      genderList
+  }
