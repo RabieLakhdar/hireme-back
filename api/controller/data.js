6aada6ad133e3095c1165ccf1659585db8dc7b12
RabieLakhdar <lakhdar.rabie.benfaraji@gmail.com>
2020-02-03 21:28:45 +0100

    use SGheet

--- api/controller/data.js
+++ api/controller/data.js
@@ -1,10 +1,6 @@
 const util = require("../util/globalFunc");
 const { genderList, durationType, priceByDuration } = require("../util/data");
-const Bearer = require("@bearer/node")("FUFbE-aEBIbyWN5aVuX3wpWVp5pMOL8C");
-const gsheet = Bearer.integration("google_sheets");
 
-const spreadsheetId = "1J_kgeaXs0xniV2Ts-W_AvUzM_BxuEF2cV3W-oDLnSXs";
-const data = [["loca", "mica"]];
 
 exports.getAllgender = (req, res, next) => {
   res
@@ -48,19 +44,4 @@ exports.getAllPriceByDuration = (req, res, next) => {
     );
 };
 
-exports.getSheet = (req, res, next) => {
 
-  res
-  .status(200)
-  .json(
-    util._request_response(
-      "POST",
-      "/sheet",
-      "add succed",
-      true,
-      null
-    )
-  );
-   
-  
-};
--- api/router/index.js
+++ api/router/index.js
@@ -1,10 +1,33 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const util = require("../util/globalFunc");
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message :"server already start"
+const Bearer = require("@bearer/node")("FUFbE-aEBIbyWN5aVuX3wpWVp5pMOL8C");
+const gsheet = Bearer.integration("google_sheets");
+
+const spreadsheetId = "1J_kgeaXs0xniV2Ts-W_AvUzM_BxuEF2cV3W-oDLnSXs";
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "server already start"
+  });
+});
+
+router.post("/sheet", (req, res, next) => {
+
+  const data = req.body.data
+  gsheet
+    .auth("546686b0-46bd-11ea-a02c-a9982b0e5ce5")
+    .post(`${spreadsheetId}/values/A1:append`, {
+      body: { values: [data] },
+      query: { valueInputOption: "RAW" }
     })
-})
+    .then(() => {
+        res
+    .status(200)
+    .json(util._request_response("POST", "/sheet", "add succed", true, null));
+    });
 
-module.exports = router
+});
+
+module.exports = router;
